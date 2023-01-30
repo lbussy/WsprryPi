d40f83218bab548823de1ec5b0e6561c94197b23
Lee Bussy <lee@bussy.org>
2023-01-30 12:18:12 -0600

    Remove macros (#3)
    * Remove PPLD from macro
    
    * Add ver and version
    
    * Move MEM_FLAG
    
    * Remove unused macro
    
    * Remove PERI_BASE_PHYS macro
    
    * Remove final macro
    
    * Use only one makefile
    
    * Reformat source

--- src/mailbox.h
+++ src/mailbox.h
@@ -1,4 +1,4 @@
-/** 
+/**
    @file mailbox.h
    @brief don't know yet
 
@@ -41,90 +41,91 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define LOCAL_DEVICE_FILE_NAME "/tmp/mbox"
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-/** @brief open a char device file used for communicated with the kernel mbox driver. A value of -1 returned from this fuction means that it was unable to open/create the kernal mbox device file
- *  @return int - the file handle descriptor
- */
-int mbox_open();
-/** @brief close the file descriptor
- *  @param file_desc - the number returned by mbox_open
- *  @return Void.
- */
-void mbox_close(int file_desc);
+    /** @brief open a char device file used for communicated with the kernel mbox driver. A value of -1 returned from this fuction means that it was unable to open/create the kernal mbox device file
+     *  @return int - the file handle descriptor
+     */
+    int mbox_open();
+    /** @brief close the file descriptor
+     *  @param file_desc - the number returned by mbox_open
+     *  @return Void.
+     */
+    void mbox_close(int file_desc);
 
-/** @brief there is no body for this function
- *  @param file_desc - the number returned by mbox_open
- *  @return uint32_t
- */
-uint32_t get_version(int file_desc);
-/** @brief write an array out to mbox_property. Calls exit if this fails
- *  @param file_desc - the number returned by mbox_open
- *  @param size - number of bytes or pages?
- *  @param align - alignment
- *  @param flags - MEM_FLAG_L1_NONALLOCATING?
- *  @return uint32_t - returns size
- */
-uint32_t mem_alloc(int file_desc, uint32_t size, uint32_t align, uint32_t flags);
-/** @brief write an array out to mbox_proptery.
- *  @param file_desc - the number returned by mbox_open
- *  @param handle - value written to mbox_property
- *  @return uint32_t - either value of handle for success or 0 for error
- */
-uint32_t mem_free(int file_desc, uint32_t handle);
-/** @brief write an array out to mbox_property. Calls exit if this fails
- *  @param file_desc - the number returned by mbox_open
- *  @param handle - value written to mbox_property
- *  @return uint32_t - value of handle 
- */
-uint32_t mem_lock(int file_desc, uint32_t handle);
-/** @brief write an array out to mbox_proptery.
- *  @param file_desc - the number returned by mbox_open
- *  @param handle - value written to mbox_property
- *  @return uint32_t - either value of handle for success or 0 for error
- */
-uint32_t mem_unlock(int file_desc, uint32_t handle);
-/** @brief open a read/write/sync file at the start of page where base is located and make the file size
- *  @param base - the base address of the file
- *  @param size - the size of the file
- *  @return (void*) - pointer to the spot in memory at the address base
- */
-void *mapmem(uint32_t base, uint32_t size);
-/** @brief call munmap on *addr and return NULL. Call exit if it fails
- *  @param addr - the address to pass to munmap
- *  @param size - the size to pass to munmap
- *  @return Void.
- */
-void unmapmem(void *addr, uint32_t size);
+    /** @brief there is no body for this function
+     *  @param file_desc - the number returned by mbox_open
+     *  @return uint32_t
+     */
+    uint32_t get_version(int file_desc);
+    /** @brief write an array out to mbox_property. Calls exit if this fails
+     *  @param file_desc - the number returned by mbox_open
+     *  @param size - number of bytes or pages?
+     *  @param align - alignment
+     *  @param flags - MEM_FLAG_L1_NONALLOCATING?
+     *  @return uint32_t - returns size
+     */
+    uint32_t mem_alloc(int file_desc, uint32_t size, uint32_t align, uint32_t flags);
+    /** @brief write an array out to mbox_proptery.
+     *  @param file_desc - the number returned by mbox_open
+     *  @param handle - value written to mbox_property
+     *  @return uint32_t - either value of handle for success or 0 for error
+     */
+    uint32_t mem_free(int file_desc, uint32_t handle);
+    /** @brief write an array out to mbox_property. Calls exit if this fails
+     *  @param file_desc - the number returned by mbox_open
+     *  @param handle - value written to mbox_property
+     *  @return uint32_t - value of handle
+     */
+    uint32_t mem_lock(int file_desc, uint32_t handle);
+    /** @brief write an array out to mbox_proptery.
+     *  @param file_desc - the number returned by mbox_open
+     *  @param handle - value written to mbox_property
+     *  @return uint32_t - either value of handle for success or 0 for error
+     */
+    uint32_t mem_unlock(int file_desc, uint32_t handle);
+    /** @brief open a read/write/sync file at the start of page where base is located and make the file size
+     *  @param base - the base address of the file
+     *  @param size - the size of the file
+     *  @return (void*) - pointer to the spot in memory at the address base
+     */
+    void *mapmem(uint32_t base, uint32_t size);
+    /** @brief call munmap on *addr and return NULL. Call exit if it fails
+     *  @param addr - the address to pass to munmap
+     *  @param size - the size to pass to munmap
+     *  @return Void.
+     */
+    void unmapmem(void *addr, uint32_t size);
 
-/** @brief write an array out to mbox_proptery.
- *  @param file_desc - the number returned by mbox_open
- *  @param code - value written to mbox_property
- *  @param r0
- *  @param r1
- *  @param r2
- *  @param r3
- *  @param r4
- *  @param r5
- *  @return uint32_t - either value of code for success or 0 for error
- */
-uint32_t execute_code(int file_desc, uint32_t code, uint32_t r0, uint32_t r1, uint32_t r2, uint32_t r3, uint32_t r4, uint32_t r5);
-/** @brief write an array out to mbox_proptery.
- *  @param file_desc - the number returned by mbox_open
- *  @param enable - value written to mbox_property
- *  @return uint32_t - either value of enable for success or 0 for error
- */
-uint32_t qpu_enable(int file_desc, uint32_t enable);
-/** @brief write an array out to mbox_proptery.
- *  @param file_desc - the number returned by mbox_open
- *  @param num_qpus
- *  @param control
- *  @param noflush
- *  @param timeout
- *  @return uint32_t - either value of num_qpus for success or 0 for error
- */
-uint32_t execute_qpu(int file_desc, uint32_t num_qpus, uint32_t control, uint32_t noflush, uint32_t timeout);
+    /** @brief write an array out to mbox_proptery.
+     *  @param file_desc - the number returned by mbox_open
+     *  @param code - value written to mbox_property
+     *  @param r0
+     *  @param r1
+     *  @param r2
+     *  @param r3
+     *  @param r4
+     *  @param r5
+     *  @return uint32_t - either value of code for success or 0 for error
+     */
+    uint32_t execute_code(int file_desc, uint32_t code, uint32_t r0, uint32_t r1, uint32_t r2, uint32_t r3, uint32_t r4, uint32_t r5);
+    /** @brief write an array out to mbox_proptery.
+     *  @param file_desc - the number returned by mbox_open
+     *  @param enable - value written to mbox_property
+     *  @return uint32_t - either value of enable for success or 0 for error
+     */
+    uint32_t qpu_enable(int file_desc, uint32_t enable);
+    /** @brief write an array out to mbox_proptery.
+     *  @param file_desc - the number returned by mbox_open
+     *  @param num_qpus
+     *  @param control
+     *  @param noflush
+     *  @param timeout
+     *  @return uint32_t - either value of num_qpus for success or 0 for error
+     */
+    uint32_t execute_qpu(int file_desc, uint32_t num_qpus, uint32_t control, uint32_t noflush, uint32_t timeout);
 #ifdef __cplusplus
 };
 #endif
