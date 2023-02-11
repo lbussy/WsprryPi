3c9639faef359a7106643843547a402ca69ef4fe
LBussy <lee@bussy.org>
2023-02-11 08:36:51 -0600

    Renamed to .hpp

--- src/config.hpp
+++ src/config.hpp
@@ -19,10 +19,6 @@ public:
     {
         return transmit;
     }
-    bool getreset()
-    {
-        return reset;
-    }
     std::string getCallsign()
     {
         return callsign;
@@ -35,25 +31,9 @@ public:
     {
         return txpower;
     }
-    std::string getFrequency1()
+    std::string getFrequency()
     {
-        return frequency1;
-    }
-    std::string getFrequency2()
-    {
-        return frequency2;
-    }
-    std::string getFrequency3()
-    {
-        return frequency3;
-    }
-    std::string getFrequency4()
-    {
-        return frequency4;
-    }
-    std::string getFrequency5()
-    {
-        return frequency5;
+        return frequency;
     }
     double getPpm()
     {
@@ -66,6 +46,14 @@ public:
     bool getOffset()
     {
         return offset;
+    }
+    bool useLED()
+    {
+        return useled;
+    }
+    bool useDaemon()
+    {
+        return usedaemon;
     }
 
 private:
@@ -86,40 +74,34 @@ private:
         {
             // Control group
             transmit = reader.GetBoolean("Control", "Transmit", false);
-            reset = reader.GetBoolean("Control", "Reset", false);
 
             // Common group
             callsign = reader.Get("Common", "Call Sign", "");
             gridsquare = reader.Get("Common", "Grid Square", "");;
             txpower = reader.GetInteger("Common", "TX Power", 0);
-            frequency1 = reader.Get("Common", "Frequency", "");
+            frequency = reader.Get("Common", "Frequency", "");
 
             // Extended Group
-            frequency2 = reader.Get("Extended", "Frequency 2", "");;
-            frequency3 = reader.Get("Extended", "Frequency 3", "");;
-            frequency4 = reader.Get("Extended", "Frequency 4", "");;
-            frequency5 = reader.Get("Extended", "Frequency 5", "");;
             ppm = reader.GetReal("Extended", "PPM", 0.0);;
             freerun = reader.GetBoolean("Extended", "Free Run", false);;
             offset = reader.GetBoolean("Extended", "Offset", false);;
+            useled = reader.GetBoolean("Extended", "Use LED", false);;
+            usedaemon = reader.GetBoolean("Extended", "Use Daemon", false);;
         }
     }
 
     bool isinitialized;
     // Control group
     bool transmit;
-    bool reset;
     // Common group
     std::string callsign;
     std::string gridsquare;
     int txpower;
+    std::string frequency;
     // Extended group
-    std::string frequency1;
-    std::string frequency2;
-    std::string frequency3;
-    std::string frequency4;
-    std::string frequency5;
     double ppm;
     bool freerun;
     bool offset;
+    bool useled;
+    bool usedaemon;
 };
