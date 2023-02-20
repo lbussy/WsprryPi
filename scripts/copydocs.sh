3c1e233ed778656110d369ac8035bea7aa2e6497
LBussy <lee@bussy.org>
2023-02-20 07:31:24 -0600

    Moved

--- scripts/copydocs.sh
+++ scripts/copydocs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-make clean
-make html
+(cd ../docs; make clean)
+(cd ../docs; make html)
 sudo rm -fr /var/www/html/wspr/docs
 sudo mkdir /var/www/html/wspr/docs
-sudo cp -R /home/pi/WsprryPi/docs/_build/html /var/www/html/wspr/docs
+sudo cp -R /home/pi/WsprryPi/docs/_build/html/* /var/www/html/wspr/docs/
 sudo chown -R www-data:www-data /var/www/html/wspr/docs
