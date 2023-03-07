b1dca447235b06e66ca916e1adb7e4c44be47d2e
Lee Bussy <lee@bussy.org>
2023-03-07 15:30:06 -0600

    Update local install work (#33)

---
+++ scripts/copyexe.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo systemctl stop wspr
+sudo systemctl stop shutdown-button
+sudo cp -f ./wspr /usr/local/bin
+sudo cp -f ./wspr.ini /usr/local/etc
+sudo systemctl start wspr
+sudo systemctl start shutdown-button
