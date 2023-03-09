5dcd8481f81bfe3c0c4424518e01d4c5f4bfa5f0
LBussy <lee@bussy.org>
2023-03-09 09:02:35 -0600

    Move logging to a lib

---
+++ unittests/logging/lcblog.hpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <string> 
+#include <regex> 
+
+class LCBLog
+{
+public:
+    void setDaemon(bool daemonmode)
+    {
+        isDaemon = daemonmode;
+    }
+
+    template <typename T>
+    void logS(T t)
+    {
+        if (isDaemon)
+            std::cout << getStamp() << "\t<I> ";
+        prtStd(t);
+    }
+
+    template<typename T, typename... Args>
+    void logS(T t, Args... args)
+    {
+        if (isDaemon)
+            std::cout << getStamp() << "\t<I> ";
+        prtStd(t, args...);
+    }
+
+    template <typename T>
+    void logE(T t)
+    {
+        if (isDaemon)
+            std::cerr << getStamp() << "\t<E> ";
+        prtStd(t);
+    }
+
+    template<typename T, typename... Args>
+    void logE(T t, Args... args)
+    {
+        if (isDaemon)
+            std::cerr << getStamp() << "\t<E> ";
+        prtStd(t, args...);
+    }
+
+private:
+    bool isDaemon;
+    std::string printline;
+
+    // Standard Out Printline
+    //
+    // Single arguments
+    void prtStd(std::string t)
+    {
+        // String argument
+        printline = printline + t;
+        crush(printline);
+        std::cout << printline << std::endl << std::flush;
+        printline = "";
+    }
+    void prtStd(const char * t)
+    {
+        // Char argument
+        printline = printline + t;
+        crush(printline);
+        std::cout << printline << std::endl << std::flush;
+        printline = "";
+    }
+    template <typename T>
+    void prtStd(T t)
+    {
+        // Numeric argument
+        printline = printline + std::to_string(t);
+        crush(printline);
+        std::cout << printline << std::endl << std::flush;
+        printline = "";
+    }
+    //
+    // Multiple arguments
+    template <typename... Args>
+    void prtStd(std::string t, Args... args)
+    {
+        // First argument is a string
+        printline = printline + t;
+        prtStd(args...);
+    }
+    template <typename... Args>
+    void prtStd(const char * t, Args... args)
+    {
+        // First argument is a char
+        printline = printline + t;
+        prtStd(args...);
+    }
+    template <typename T, typename... Args>
+    void prtStd(T t, Args... args)
+    {
+        // First argument is numeric
+        printline = printline + std::to_string(t);
+        prtStd(args...);
+    }
+
+    // Standard Error Printline
+    //
+    // Single arguments
+    void prtErr(std::string t)
+    {
+        // String argument
+        printline = printline + t;
+        crush(printline);
+        std::cerr << printline << std::endl << std::flush;
+        printline = "";
+    }
+    void prtErr(const char * t)
+    {
+        // Char argument
+        printline = printline + t;
+        crush(printline);
+        std::cerr << printline << std::endl << std::flush;
+        printline = "";
+    }
+    template <typename T>
+    void prtErr(T t)
+    {
+        // Numeric argument
+        printline = printline + std::to_string(t);
+        crush(printline);
+        std::cerr << printline << std::endl << std::flush;
+        printline = "";
+    }
+    //
+    // Multiple arguments
+    template <typename... Args>
+    void prtErr(std::string t, Args... args)
+    {
+        // First argument is a string
+        printline = printline + t;
+        prtErr(args...);
+    }
+    template <typename... Args>
+    void prtErr(const char * t, Args... args)
+    {
+        // First argument is a char
+        printline = printline + t;
+        prtErr(args...);
+    }
+    template <typename T, typename... Args>
+    void prtErr(T t, Args... args)
+    {
+        // First argument is numeric
+        printline = printline + std::to_string(t);
+        prtErr(args...);
+    }
+
+    void crush(std::string &s)
+    {
+        s = std::regex_replace(s, std::regex("\\s+"), " ");
+        s = std::regex_replace(s, std::regex("^\\s+|\\s+$"), "");
+    }
+
+    std::string getStamp()
+    {
+        char dts[20];
+        time_t t = time(0);
+        struct tm *tm;
+
+        tm = gmtime(&t);
+        strftime(dts, sizeof(dts), "%F %T", tm); 
+        std::string str(dts);
+        return str;
+    }
+};
