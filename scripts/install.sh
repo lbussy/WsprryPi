c255df1d3230532612530651f2798ab4b3975252
lbussy <lee@bussy.org>
2024-11-18 14:58:47 -0600

    Update headers

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copyright (C) 2023-2024 Lee C. Bussy (@LBussy)
-# Created for WsprryPi project, version 1.2.1-2d33a85 [new_release_proc].
+# Created for WsprryPi project, version 1.2.1-5b50d3c [new_release_proc].
 
 ############
 ### Global Declarations
