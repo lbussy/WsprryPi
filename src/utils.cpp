58bcedf4bfebe24a8a01df7752336a6c0845da2f
LBussy <lee@bussy.org>
2024-11-22 06:15:00 -0600

    Move utils to separate source files

---
+++ src/utils.cpp
@@ -0,0 +1,32 @@
+// This file is released under the GPL v3 License, see <https://www.gnu.org/licenses/>.
+
+/*
+ * WsprryPi
+ * Updated and maintained by Lee C. Bussy
+ *
+ * Originally forked from threeme3/WsprryPi (no longer active), this
+ * project has been significantly updated, improved, and documented for
+ * ease of use.
+ *
+ * Inspired by a conversation with Bruce Raymond of TAPR, this fork has
+ * diverged substantially from its origins and operates as an independent
+ * project.
+ *
+ * Contributors:
+ *   - threeme3 (Original Author)
+ *   - Bruce Raymond (Inspiration and Guidance)
+ *   - Lee Bussy, aa0nt@arrl.net
+ *
+ * Copyright (C) 2023-2024 Lee C. Bussy (@LBussy). All rights reserved.
+ *
+ * This code is part of Lee Bussy's WsprryPi project, version 1.2.1-55ad7f3 [fix_57].
+*/
+
+#include "utils.hpp"
+
+void to_upper(char* str) {
+    while (*str) {
+        *str = static_cast<char>(std::toupper(*str)); // Safely cast to char
+        ++str;
+    }
+}
