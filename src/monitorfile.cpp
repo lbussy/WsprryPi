5659b79505b003aaade39f2f8b01d4e9e5e62d2d
LBussy <lee@bussy.org>
2024-11-25 05:53:04 -0600

    Unit test and doxygen comments

---
+++ src/monitorfile.cpp
@@ -0,0 +1,82 @@
+// This file is released under the GPL v3 License, see <https://www.gnu.org/licenses/>.
+
+/*
+ * WsprryPi
+ * Updated and maintained by Lee C. Bussy
+ *
+ * Originally forked from threeme3/WsprryPi (no longer active), this
+ * project has been significantly updated, improved, and documented for
+ * ease of use.
+ *
+ * Inspired by a conversation with Bruce Raymond of TAPR, this fork has
+ * diverged substantially from its origins and operates as an independent
+ * project.
+ *
+ * Contributors:
+ *   - threeme3 (Original Author)
+ *   - Bruce Raymond (Inspiration and Guidance)
+ *   - Lee Bussy, aa0nt@arrl.net
+ *
+ * Copyright (C) 2023-2024 Lee C. Bussy (@LBussy). All rights reserved.
+ *
+ * This code is part of Lee Bussy's WsprryPi project, version 1.2.1-32d490b [refactoring].
+ */
+
+// Unit tests:
+// g++ -std=c++17 -DDEBUG_MAIN_MONITORFILE monitorfile.cpp -o monitorfile
+// Test command:
+// ./monitorfile
+
+#include "monitorfile.hpp"
+#include <fstream> // Required for std::ofstream
+#include <thread>
+#include <chrono>
+
+#ifdef DEBUG_MAIN_MONITORFILE
+
+/**
+ * @brief Main function for debugging and testing the MonitorFile class.
+ */
+int main()
+{
+    MonitorFile monitor;
+    const std::string testFileName = "testfile.txt";
+
+    // Create or modify the test file for demonstration purposes
+    std::ofstream testFile(testFileName);
+    testFile << "Initial content.\n";
+    testFile.close();
+
+    monitor.filemon(testFileName);
+
+    std::cout << "Monitoring file: " << testFileName << "\n";
+
+    for (int i = 0; i < 5; ++i)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+
+        // Modify the file to trigger a change
+        if (i % 2 == 1)
+        {
+            std::ofstream modifyFile(testFileName, std::ios::app);
+            modifyFile << "Modification " << i << "\n";
+            modifyFile.close();
+        }
+
+        if (monitor.changed())
+        {
+            std::cout << "File has been modified!\n";
+        }
+        else
+        {
+            std::cout << "No changes detected.\n";
+        }
+    }
+
+    // Cleanup: Delete the test file
+    fs::remove(testFileName);
+
+    return 0;
+}
+
+#endif // DEBUG_MAIN_MONITORFILE
--- src/monitorfile.hpp
+++ src/monitorfile.hpp
@@ -22,23 +22,35 @@
  * This code is part of Lee Bussy's WsprryPi project, version 1.2.1-32d490b [refactoring].
  */
 
-#ifndef MONITORFILE_H
-#define MONITORFILE_H
+#ifndef _MONITORFILE_HPP
+#define _MONITORFILE_HPP
 
 #include <filesystem>
 #include <iostream>
 
 namespace fs = std::filesystem;
 
+/**
+ * @class MonitorFile
+ * @brief Monitors a file for changes based on its last write time.
+ */
 class MonitorFile
 {
 public:
+    /**
+     * @brief Initializes file monitoring for a specified file.
+     * @param fileName The name of the file to monitor.
+     */
     void filemon(const std::string &fileName)
     {
         file_name = fileName.c_str();
         start_monitoring();
     }
 
+    /**
+     * @brief Checks if the monitored file has been modified since the last check.
+     * @return `true` if the file has changed; otherwise, `false`.
+     */
     bool changed()
     {
         check_file();
@@ -54,19 +66,25 @@ public:
     }
 
 private:
+    /**
+     * @brief Initializes the monitoring process by recording the file's current last write time.
+     */
     void start_monitoring()
     {
         org_time = fs::last_write_time(file_name);
     }
 
+    /**
+     * @brief Updates the recorded last write time of the monitored file.
+     */
     void check_file()
     {
         new_time = fs::last_write_time(file_name);
     }
 
-    std::string file_name;
-    fs::file_time_type org_time;
-    fs::file_time_type new_time;
+    std::string file_name;           ///< Name of the file being monitored.
+    fs::file_time_type org_time;     ///< Original recorded last write time of the file.
+    fs::file_time_type new_time;     ///< Updated last write time of the file.
 };
 
-#endif // MONITORFILE_H
+#endif // _MONITORFILE_HPP
