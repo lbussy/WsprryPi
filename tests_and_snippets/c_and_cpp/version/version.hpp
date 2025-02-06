994da3bb873359da857a1ed5cc24d990ad608900
“lbussy” <lee@bussy.org>
2025-02-06 06:52:51 -0600

    Version without bcm libs

---
+++ tests_and_snippets/c_and_cpp/version/version.hpp
@@ -0,0 +1,51 @@
+/**
+ * @file version.cpp
+ * @brief Provides software and hardware version information.
+ *
+ * This file is part of WsprryPi, a project originally forked from
+ * threeme3/WsprryPi (no longer active on GitHub).
+ *
+ * However, this new code added to the project is distributed under under
+ * the MIT License. See LICENSE.MIT.md for more information.
+ *
+ * Copyright (C) 2023-2025 Lee C. Bussy (@LBussy). All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#include <string>
+
+// Function prototype for getRaspberryPiModel
+inline const char* getRaspberryPiModel();
+inline const char* getProcessorType();
+inline const char* getAppVersion();
+inline const char* exe_version();
+inline const char* branch();
+inline const char* exe_name();
+inline const char* project_name();
+inline int getProcessorTypeAsInt();
+
+static unsigned get_dt_ranges(const char *filename, unsigned offset);
+unsigned bcm_host_get_peripheral_address(void);
+unsigned get_peripheral_address(void);
+
+#endif // VERSION_H
