1ce67746de9c5e4cba2b9b2cb63ddd8d8782bea2
lbussy <lee@bussy.org>
2025-03-10 06:55:21 -0500

    Split out DMA work

---
+++ tests_and_snippets/c_and_cpp/dma/dma.hpp
@@ -0,0 +1,16 @@
+#ifndef _WSPR_H
+#define _WSPR_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#include "mailbox.h"
+}
+#endif /* __cplusplus */
+
+void dma_cleanup();
+void setup_dma();
+void tx_tone();
+void tx_wspr();
+
+#endif // _WSPR_H
