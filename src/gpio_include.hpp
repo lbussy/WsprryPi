e8977c0e85d1e9473df7b794dcbb344d91d3a173
lbussy <lee@bussy.org>
2026-01-26 06:45:24 -0600

    More aggressive clamping of jobs on compile

--- src/gpio_include.hpp
+++ src/gpio_include.hpp
@@ -25,15 +25,15 @@
 #  define GPIOD_API_MAJOR 1
 #endif
 
-#ifdef DEBUG_WSPR
-#  ifndef __PRINTED_GPIOD_VERSION
-#    define __PRINTED_GPIOD_VERSION 1
-#    if GPIOD_API_MAJOR >= 2
-#      pragma message "Compiling with libgpiod v2 API"
-#    else
-#      pragma message "Compiling with libgpiod v1 API"
-#    endif
-#  endif
-#endif
+// #ifdef DEBUG_WSPR
+// #  ifndef __PRINTED_GPIOD_VERSION
+// #    define __PRINTED_GPIOD_VERSION 1
+// #    if GPIOD_API_MAJOR >= 2
+// #      pragma message "Compiling with libgpiod v2 API"
+// #    else
+// #      pragma message "Compiling with libgpiod v1 API"
+// #    endif
+// #  endif
+// #endif
 
 #endif // GPIO_INCLUDE_HPP
